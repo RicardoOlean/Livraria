dbfa8fa43e1bb1bf9d53894c4490dcc156f5f841
Ricardo Oliveira Leandro <48275184835@>
2025-10-24 11:23:41 -0300

    Filtragem

--- back/api/views.py
+++ back/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework_simplejwt.tokens import RefreshToken
-from .filters import AutorFilter
+from .filters import AutorFilter, EditoraFilter, LivroFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
@@ -31,10 +31,12 @@ class AutoresView(ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
     # permission_classes =[IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    # filterset_fields = ['id']               # Permite o filtro exato
-    # search_fields = ['nome', 'sobrenome']   # busca parcial: ?search=Jorge
-    filterset_class = AutorFilter           # Caso queira filtro duplo "Nome" e "Nacionalidade"
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['id']               
+    search_fields = ['autor', 's_autor', 'nasc']  
+    ordering_fields = ['id', 'autor']
+    ordering = ['autor']
+    filterset_class = AutorFilter           
     
 class AutoresDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
@@ -45,9 +47,12 @@ class EditorasView(ListCreateAPIView):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
     # permission_classes =[IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ['id', 'nome']
-    search_fields = ['nome']  
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['id', 'editora']
+    search_fields = ['editora']
+    ordering_fields = ['id', 'editora']
+    ordering = ['editora'] 
+    filterset_class = EditoraFilter
 
 class EditorasDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Editora.objects.all()
@@ -55,12 +60,15 @@ class EditorasDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes =[IsAuthenticated]
 
 class LivrosView(ListCreateAPIView):
-    queryset = Livro.objects.all().select_related('autor')
+    queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    # permission_classes =[IsAuthenticated]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    search_fields = ['titulo', 'autor__nome', 'autor__sobrenome']
+    filterset_fields = ['id']
+    search_fields = ['titulo', 'subtitulo']
     ordering_fields = ['id', 'titulo']
     ordering = ['titulo']
+    filterset_class = LivroFilter  
 
 
 class LivrosDetailView(RetrieveUpdateDestroyAPIView):
@@ -69,7 +77,7 @@ class LivrosDetailView(RetrieveUpdateDestroyAPIView):
     # permission_classes =[IsAuthenticated]
 
 class LivroViewSet(ModelViewSet):
-    queryset = Livro.objects.all().select_related('autor')
+    queryset = Livro.objects.all()
     serializer_class = LivroSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # se você criou o LivroFilter com autor/titulo:
@@ -96,29 +104,17 @@ class ImagemViewSet(ModelViewSet):
     serializer_class = ImagemSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.parsers import MultiPartParser, FormParser
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-
-from .models import Livro
-from .serializers import LivroSerializer
-
-
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.select_related("autor").order_by("-id")
     serializer_class = LivroSerializer
     parser_classes = [MultiPartParser, FormParser]
 
     # filtros / busca / ordenação
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    search_fields = ["titulo", "autor__nome", "autor__sobrenome"]
+    # filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # search_fields = ["titulo", "autor__nome", "autor__sobrenome"]
     ordering_fields = ["id", "titulo"]
     ordering = ["titulo"]
+
 
     @action(detail=True, methods=["post"], parser_classes=[MultiPartParser, FormParser])
     def capa(self, request, pk=None):
